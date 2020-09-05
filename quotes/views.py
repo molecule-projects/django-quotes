9acb0e77152d74c8284e5643539201d01a629901
dropscar <dropscar041205@gmail.com>
2020-09-05 22:46:12 +0900

    delete comment

--- quotes/views.py
+++ quotes/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 
-from .models import Quote  # new
+from .models import Quote
 
 
 def home_page_view(request):
     template_name = 'quotes/home.html'
-    queryset = Quote.objects.all()  # new
-    # new
+    queryset = Quote.objects.all()
     context = {
         'object_list': queryset
     }
-    return render(request, template_name, context)  # new
+    return render(request, template_name, context)
 
 
 def about_page_view(request):
